3a3f617eb77a45cfe8f3fda5fe7086e3db842074
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-09 12:08:18 +0400

    Add files via upload

---
+++ CorejavaBrush1.java
@@ -0,0 +1,16 @@
+
+public class CorejavaBrush1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 1;
+		String name = ("Dubai UAE  United Kingdom");
+		char C = 'a' ;
+		
+		
+		System.out.println("This is number " + number);
+
+	}
+
+}
