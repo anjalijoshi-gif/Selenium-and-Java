91370dbc32f050d76432518100402a32ebd85785
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-09 17:19:30 +0400

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,34 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int[] arr1 = new int[6];
+		
+		//for Loop
+		for(int i = 0;i < arr.length; i++)
+		{
+			
+			System.out.println(i);
+		
+		
+			
+		}
+		
+		for (int i = 0; i< arr1.length ; i++)
+		{
+			System.out.println(i);
+		}
+		
+		String[] name = {"anjali","QA" , "UAE"};
+		
+		for(int i= 0; i<name.length; i++)
+		{
+			System.out.println(name[i]);
+		}
+
+	}
+
+}
