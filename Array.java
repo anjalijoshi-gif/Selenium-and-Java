32babeedb53dc04c37ee10ad209d0a6bd0b4ff44
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-09 16:44:40 +0400

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,23 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = new int[6];
+		arr[0] = 1;
+		arr[1] = 4;
+	//	arr[7] = 6;
+		
+	//	System.out.println(arr[7]);  // This will throw error out of boundsException
+		
+		int[] arr1 = {1,2,3,4}; 
+		
+		System.out.println(arr1[0]);
+		
+		
+		
+
+	}
+
+}
