0f2398aa86ed07dc390b5d992a26a8879936c010
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-09 18:11:27 +0400

    Add files via upload

---
+++ Enhanced.java
@@ -0,0 +1,17 @@
+
+public class Enhanced {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] name = {"Testing","Automation", "Selenium"};
+		
+		for(String s : name)
+		{
+			System.out.println((s));
+			
+		}
+
+	}
+
+}
