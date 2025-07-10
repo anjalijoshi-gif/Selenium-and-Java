5097776101dc2e74be4bdebf6bd6959154ed7b12
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-10 09:53:44 +0400

    Add files via upload

---
+++ IFcondition.java
@@ -0,0 +1,23 @@
+
+public class IFcondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr1 = {1,2,3,4,5,12,16,18,20,22};
+		
+		for(int i = 0; i < arr1.length ; i++)
+			if(arr1[i] % 2 == 0)
+			
+		{
+			System.out.println(arr1[i]);
+		}
+			else
+			{
+			System.out.println("not divide by 2");
+				
+			}
+
+	}
+
+}
