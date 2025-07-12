01190cf48683744a99410d880adfa50ec3905c7c
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-12 11:52:09 +0400

    Add files via upload

---
+++ MethodsDemo.java
@@ -0,0 +1,23 @@
+
+public class MethodsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodsDemo aa = new MethodsDemo();
+		String name = aa.getData();
+		System.out.println(name);
+		
+		MethodsDemo2 aa1 = new MethodsDemo2();
+		aa1.getData1();
+		
+
+	}
+	
+	public String getData()
+	{
+		System.out.println("Hello World");
+		return "Anjali Joshi";
+	}
+
+}
---
+++ MethodsDemo2.java
@@ -0,0 +1,12 @@
+
+public class MethodsDemo2 
+{
+	
+	public void getData1() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("UAE");
+		
+	}
+}
+
