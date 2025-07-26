32b0ae258784a76179bc7241e5bbc56e2265aa92
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-07-26 11:32:19 +0400

    Add files via upload

---
+++ Swapping1.java
@@ -0,0 +1,22 @@
+
+public class Swapping1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 22;
+		int b = 32;
+		
+		int t = a;
+		
+		System.out.println("Value of a " +a +b +t);
+		
+		a = b*t;
+		b = a+t;
+		
+		System.out.println(a);
+		System.out.println(t);
+
+	}
+
+}
